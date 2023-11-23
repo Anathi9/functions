da01131d67977521fc94256de72754cec1c6883f
Anathi9 <anathijosefu@gmail.com>
2023-11-23 14:58:17 +0200

    changes

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function greet(name) {
+    console.log("Hello" + name);
+}
+greet("Anathi")
+
+
+function multiply(y,z) {
+    console.log(y * z);
+}
+multiply(5, 5);
+
+
+function tableOfFive(number) {
+    let output = number * 5;
+    console.log(output);
+}
+tableOfFive(7)
